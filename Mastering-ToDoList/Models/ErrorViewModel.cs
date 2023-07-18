ac1b762bfc0dada2571ae319d03f4ef1bab7225e
Dilacerare <65394791+Dilacerare@users.noreply.github.com>
2023-07-18 20:47:32 +0500

    Add first things

--- Mastering-ToDoList/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Mastering_ToDoList.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
---
+++ ToDoList.Domain/Enum/Priority.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Domain.Enum;
+
+public enum Priority
+{
+    [Display(Name = "Простая")]
+    Easy = 1,
+    [Display(Name = "Средняя")]
+    Medium = 2,
+    [Display(Name = "Сложная")]
+    Hard = 3
+    
+}
